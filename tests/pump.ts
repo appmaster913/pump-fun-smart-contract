e3044f38756f7cd7a0f421fb24a8ef440c03f3e5
John William <jw7889208@gmail.com>
2025-01-07 11:15:35 +0900

    update

--- tests/pump.ts
+++ tests/pump.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Pump } from "../target/types/pump"
-
+console.log('p')
 describe("pump", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
